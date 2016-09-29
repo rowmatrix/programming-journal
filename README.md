6ce29e4b3fc9490e9a0ee8ed5ca5894e12bf8fe3
rowmatrix <romayibar@gmail.com>
2016-09-28 23:32:19 -0700

    Initial Entry

---
+++ README.md
@@ -0,0 +1,3 @@
+# Programming Journal
+
+This repo will act as a programming journal or diary. This journal will keep a collection of solutions to coding challenges and exercises from various sources.
---
+++ read_stdin.py
@@ -0,0 +1,17 @@
+'''
+Task
+Read a line of input from stdin and save it to a variable, s. Then print the contents of to stdout.
+
+Input Format
+
+A single line containing sentence .
+
+Constraints
+
+Output Format
+
+Print the contents of to stdout.
+'''
+
+s = input()
+print(s)
